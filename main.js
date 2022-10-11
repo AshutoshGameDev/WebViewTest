f040e80c7392caf43731c4fdfa4973d6f398e02b
विद्रोही <103189075+AshutoshGameDev@users.noreply.github.com>
2022-10-11 16:34:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
 function pay() {
     document.getElementById("staus-label").innerText = "payment success";
-    Unity.call('form?pay=' + "payment success");
+    Unity.call("payment success");
     var msg = document.getElementById("msg");
 }
 
 function fail() {
     document.getElementById("staus-label").innerText = "payment failed";
-    Unity.call('form?fail=' + "payment failed");
+    Unity.call("payment failed");
     var msg = document.getElementById("msg");
 }
